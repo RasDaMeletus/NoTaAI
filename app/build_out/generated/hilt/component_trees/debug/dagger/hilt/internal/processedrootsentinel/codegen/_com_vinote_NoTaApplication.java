80ab8d6d04a31fe9c2719f0d07ea716358727244
RasDaMeletus <farrassyafiq213@gmail.com>
2026-09-16 06:23:13 +0700

    chore: remove build_out from git tracking

--- app/build_out/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_vinote_NoTaApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.vinote.NoTaApplication"
-)
-public final class _com_vinote_NoTaApplication {
-}
